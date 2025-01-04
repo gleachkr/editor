8941e4ee608dde4b239913e804c3e2207a3bf13b
Graham <gleachkr@gmail.com>
2025-01-04 14:56:25 -0500

    Remove some mappings now covered by defaults

--- neovim/plugin/lsp.lua
+++ neovim/plugin/lsp.lua
@@ -1,19 +1,13 @@
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
     -- Mappings.
-    local opts = { noremap = true, silent = true }
+    local opts = { noremap = true, silent = true, buffer = bufnr }
 
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap('n', '<LocalLeader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', '<LocalLeader>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+    vim.keymap.set('n', '<LocalLeader>ca', vim.lsp.buf.code_action, opts)
 end
+
 --setup neodev
 
 require("neodev").setup()
