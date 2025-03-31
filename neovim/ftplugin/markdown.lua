2deecc92f07699f39307f9f1af470b126518ab49
Graham <gleachkr@gmail.com>
2025-03-31 06:51:18 -0400

    refine markdown settings

--- neovim/ftplugin/markdown.lua
+++ neovim/ftplugin/markdown.lua
@@ -1,4 +1,9 @@
-vim.cmd [[ColorScheme pencil]]
+if vim.fn.win_gettype() ~= "popup" then
+    vim.cmd [[ColorScheme pencil]]
+end
+
+vim.opt_local.formatoptions='tanqwl'
+vim.opt_local.formatlistpat = [[^\s*\([-*+∙∘•]\|\d\+\.\)\s\+]] 
 vim.opt_local.background="light"
 vim.opt_local.spell=true
 vim.opt_local.so=10
