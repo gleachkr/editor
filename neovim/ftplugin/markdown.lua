9a11750ad1dea9148535a70591bc6b8d2fdd1d9a
Graham Leach-Krouse <gleachkr@gmail.com>
2025-04-01 20:38:56 -0400

    Fix hover popup markdown issue

--- neovim/ftplugin/markdown.lua
+++ neovim/ftplugin/markdown.lua
@@ -1,10 +1,11 @@
 if vim.fn.win_gettype() ~= "popup" then
+    --we gate these options since most hover popups are ft=markdown
     vim.cmd [[ColorScheme pencil]]
+    vim.opt_local.background="light"
 end
 
 vim.opt_local.formatoptions='tanqwl'
 vim.opt_local.formatlistpat = [[^\s*\([-*+∙∘•]\|\d\+\.\)\s\+]] 
-vim.opt_local.background="light"
 vim.opt_local.spell=true
 vim.opt_local.so=10
 vim.opt_local.lsp=5
