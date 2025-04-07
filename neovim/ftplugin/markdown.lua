315ff34d3de077e4bf0af61e5cd4fe81af28bbe6
Graham <gleachkr@gmail.com>
2025-04-07 19:02:12 -0400

    Update lectic.nvim

--- neovim/ftplugin/markdown.lua
+++ neovim/ftplugin/markdown.lua
@@ -5,7 +5,7 @@ if vim.fn.win_gettype() ~= "popup" then
 end
 
 vim.opt_local.formatoptions='tanqwl'
-vim.opt_local.formatlistpat = [[^\s*\([-*+∙∘•]\|\d\+\.\)\s\+]] 
+vim.opt_local.formatlistpat = [[^\s*\([-*+∙∘•]\|\d\+\.\)\s\+]]
 vim.opt_local.spell=true
 vim.opt_local.so=10
 vim.opt_local.lsp=5
