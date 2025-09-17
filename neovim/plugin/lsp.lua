c3f70c6dba5225fb981ebc15ee3c09a3fdf6e5f2
Graham <gleachkr@gmail.com>
2025-09-17 18:46:23 -0400

    Switch to new lsp handlers

--- neovim/plugin/lsp.lua
+++ neovim/plugin/lsp.lua
@@ -1,43 +1,24 @@
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(_, bufnr)
-    -- Mappings.
-    local opts = { noremap = true, silent = true, buffer = bufnr }
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<LocalLeader>ca', vim.lsp.buf.code_action, opts)
-end
-
 --setup neodev
 
 require("lazydev").setup()
 
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-local generic = {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = {
-        debounce_text_changes = 150,
-    }
-}
-
-require 'lspconfig'.racket_langserver.setup(generic)
-require 'lspconfig'.bashls.setup(generic)
-require 'lspconfig'.clangd.setup(generic)
-require 'lspconfig'.cssls.setup(generic)
-require 'lspconfig'.dafny.setup(generic)
-require 'lspconfig'.hls.setup(generic)
-require 'lspconfig'.html.setup(generic)
-require 'lspconfig'.jedi_language_server.setup(generic)
-require 'lspconfig'.jsonls.setup(generic)
-require 'lspconfig'.lua_ls.setup(generic)
-require 'lspconfig'.nil_ls.setup(generic)
-require 'lspconfig'.ocamllsp.setup(generic)
-require 'lspconfig'.pylsp.setup(generic)
-require 'lspconfig'.rust_analyzer.setup(generic)
-require 'lspconfig'.texlab.setup(generic)
-require 'lspconfig'.ts_ls.setup(generic)
+vim.lsp.enable("racket_langserver")
+vim.lsp.enable("bashls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("cssls")
+vim.lsp.enable("dafny")
+vim.lsp.enable("hls")
+vim.lsp.enable("html")
+vim.lsp.enable("jedi_language_server")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("nil_ls")
+vim.lsp.enable("ocamllsp")
+vim.lsp.enable("pylsp")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("texlab")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable('pyright')
 
 vim.api.nvim_create_autocmd("FileType", {
     callback = function() vim.lsp.start({
