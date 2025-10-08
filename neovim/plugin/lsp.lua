70e7f059a3081586526d0a7fe282ada92e68d60a
Graham <gleachkr@gmail.com>
2025-10-07 21:48:45 -0400

    Bump lectic

--- neovim/plugin/lsp.lua
+++ neovim/plugin/lsp.lua
@@ -29,16 +29,6 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "quint"
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-    callback = function() vim.lsp.start({
-        name = 'lectic',
-        cmd = {'lectic', 'lsp'},
-        root_dir = vim.fs.root(0, { ".git", "lectic.yaml" }) or vim.fn.getcwd(),
-        single_file_support = true,
-    }) end,
-    pattern = { "lectic", "lectic.markdown", "markdown.lectic" }
-})
-
 -- require 'coq-lsp'.setup {
 --     lsp = {
 --         on_attach = on_attach,
