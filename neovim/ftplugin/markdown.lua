defffc9a4013480fc897e296a63ff3a3b4d5bf56
Graham <gleachkr@gmail.com>
2026-01-08 19:02:19 -0500

    Gate more markdown options

--- neovim/ftplugin/markdown.lua
+++ neovim/ftplugin/markdown.lua
@@ -2,11 +2,10 @@ if vim.fn.win_gettype() ~= "popup" then
     --we gate these options since most hover popups are ft=markdown
     vim.cmd [[ColorScheme pencil]]
     vim.opt_local.background="light"
+    vim.opt_local.spell=true
+    vim.opt_local.formatoptions='tanqwl'
+    vim.opt_local.formatlistpat = [[^\s*\([-*+∙∘•]\|\d\+\.\)\s\+]]
+    vim.opt_local.so=10
+    vim.opt_local.lsp=5
+    vim.cmd[[silent compiler pandoc]]
 end
-
-vim.opt_local.formatoptions='tanqwl'
-vim.opt_local.formatlistpat = [[^\s*\([-*+∙∘•]\|\d\+\.\)\s\+]]
-vim.opt_local.spell=true
-vim.opt_local.so=10
-vim.opt_local.lsp=5
-vim.cmd[[silent compiler pandoc]]
